ad0d9d981751abd6e75e8f0071b5a787bace53cb
rossoj85 <jasonrosso@gamil.com>
2018-12-26 15:39:49 -0600

    delete mutation

--- client/components/songCreate.js
+++ client/components/songCreate.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {graphql} from 'react-apollo';
 import gql from 'graphQL-tag';
-
+import {Link, hashHistory} from 'react-router'
+import query from '../queries/fetchsongs'
 
 
 class SongCreate extends Component {
@@ -15,17 +16,15 @@ class SongCreate extends Component {
         evt.preventDefault();
         console.log('SUBMIt FIRED')
         this.props.mutate({
-            variables: {
-                title: this.state.title
-            }
-        });
-        this.setState({title:''})
-        
+            variables: {title: this.state.title},
+            refetchQueries: [{query}]
+        }).then(() => hashHistory.push('/') ) 
     }
     render(){
        console.log(this.state.title)
         return(
             <div className>
+                <Link to='/'>Back</Link>
                 <h3>Create a new SOng</h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label><h5>Song Title:</h5></label>
--- client/components/songlist.js
+++ client/components/songlist.js
@@ -1,35 +1,58 @@
 import React, {Component} from 'react';
 import gql from 'graphQL-tag';
-import { graphQL, graphql } from 'react-apollo'
+import { graphQL, graphql } from 'react-apollo';
+import { Link } from 'react-router';
+import query from '../queries/fetchsongs'
+
 
  class Songlist extends Component{
 
+    onSongDelete(id){
+        console.log('Delete clicked')
+        this.props.mutate( { 
+            variables:{id},
+            refetchQueries: [{query}]
+        })
+    }
     renderSongs(){
-        return this.props.data.songs.map(song=>{
+        return this.props.data.songs.map(({id, title})=>{
             return(
-                <li key={song.id} className="collection-item">
-                    {song.title}
-                </li>
+                <div key={id}>
+                    <li className="collection-item">
+                        {title}
+                        <i className='material-icons' onClick ={()=>this.onSongDelete(id)}> delete </i> 
+                    </li>
+                </div>
             )
         })
     }
     render(){
        if(this.props.data.loading) {return <div>Loading...</div>};
         return(
-        <ul className="collection">
-            {this.renderSongs()}
-        </ul>
+            <div>
+                <ul className="collection">
+                    {this.renderSongs()}
+                </ul>
+                <Link to='songs/new'
+                    className= "btn-floating btn-large red right"
+                >
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
+        
         )
     }
 }
 
-const query = gql`
-{
-    songs{
-        id
-        title
+//since we only gonan use this mutation in this file, lets just define it here
+const mutation = gql`
+    mutation DeleteSong($id: ID){
+        deleteSong(id:$id){
+            id
     }
 }
-`
+`;
 
-export default graphql(query) (Songlist);
+export default graphql(mutation)(
+graphql(query) (Songlist)
+);
